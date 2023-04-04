66b6ec27134b2a29269e8e1d63e933f84f1bf9e5
Stefan Djikic <stefandjikic@outlook.com>
2023-04-04 22:13:20 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = (): JSX.Element => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello 🎉</Text>
+    </View>
+  );
+};
+
+export default App;
