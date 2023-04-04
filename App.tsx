9f32c760d7746e4bf847d9e658db7cb1dfeaa015
Stefan Djikic <stefandjikic@outlook.com>
2023-04-04 22:45:25 +0200

    add tmdb api and ts for env

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
+import {getPopularMovies} from './services/api';
+import {IMovie} from './types/interfaces';
 
 const App = (): JSX.Element => {
+  const [movie, setMovie] = useState<IMovie>({} as IMovie);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getPopularMovies()
+      .then(movies => {
+        setMovie(movies[0]);
+      })
+      .catch(err => {
+        setError(err);
+      });
+  }, []);
+
   return (
     <View
       style={{
@@ -9,7 +24,10 @@ const App = (): JSX.Element => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Hello 🎉</Text>
+      <Text>Movie Name: {movie.original_title}</Text>
+      <Text>Language: {movie.original_language}</Text>
+      <Text>Release Date: {movie.release_date}</Text>
+      {error && <Text style={{color: 'red'}}>Error in the server</Text>}
     </View>
   );
 };
---
+++ services/api.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import {API_KEY} from '@env';
+
+const apiUrl = 'https://api.themoviedb.org/3';
+const apiKey = `api_key=${API_KEY}`;
+// Get Popular Movies
+export const getPopularMovies = async () => {
+  const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
+  return resp.data.results;
+};
+
+// Get Upcoming Movies
+export const getUpcomingMovies = async () => {
+  const resp = await axios.get(`${apiUrl}/movie/upcoming?${apiKey}`);
+  return resp.data.results;
+};
+
+// Get Popular TV
+export const getPopularTv = async () => {
+  const resp = await axios.get(`${apiUrl}/tv/popular?${apiKey}`);
+  return resp.data.results;
+};
